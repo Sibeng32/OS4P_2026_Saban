fa485322e620a4faf32b806cbb2e3b1e5ca2a123
Sibeng32 <94640388+Sibeng32@users.noreply.github.com>
2026-09-10 15:48:46 +0200

    first file

---
+++ simulation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 15:46:46 2026
+
+@author: sab
+"""
+
+# Simulation of a falling body on Earth
+ACCELERATION_G = 10 # m/s^2
+TIME = 5.0 # seconds
+
+def calculate_displacement(g, t):
+    return 0.5 * g * (t ** 2)
+
+
+print(f"Displacement after {TIME}s: {calculate_displacement(ACCELERATION_G, TIME)} meters")
